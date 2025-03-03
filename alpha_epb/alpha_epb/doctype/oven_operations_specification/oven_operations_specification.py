2af0e26785dd3ddfc34477f111a8350b10888de6
iyyanarr <iyyanarr@alphaworkz.com>
2025-03-03 18:13:44 +0530

    Oven specification api

--- alpha_epb/alpha_epb/doctype/oven_operations_specification/oven_operations_specification.py
+++ alpha_epb/alpha_epb/doctype/oven_operations_specification/oven_operations_specification.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, AlphaWorkz and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe import whitelist
 
 class OvenOperationsSpecification(Document):
-	pass
+    pass
+
+@frappe.whitelist()
+def get_oven_operations_specification_by_batch(batch_no):
+    # Find the item linked with the batch
+    item = frappe.db.get_value('Batch', {'name': batch_no}, 'item')
+
+    if not item:
+        frappe.throw(f"Item not found for batch number: {batch_no}")
+
+    # Retrieve the OvenOperationsSpecification document by the item
+    oven_operations_specification = frappe.get_all('Oven Operations Specification', filters={'product': item})
+
+    if not oven_operations_specification:
+        frappe.throw(f"Oven Operations Specification not found for item: {item}")
+
+    return oven_operations_specification
