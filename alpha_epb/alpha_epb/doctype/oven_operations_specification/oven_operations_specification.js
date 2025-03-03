7b0e2f593b7c04091b52f2fa12acb653bd01f67b
Iyyanar Ranganathan <iyyanar@Iyyanars-MacBook-Air.local>
2025-03-03 14:00:34 +0530

    doctype Init

---
+++ alpha_epb/alpha_epb/doctype/oven_operations_specification/oven_operations_specification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, AlphaWorkz and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Oven Operations Specification", {
+// 	refresh(frm) {
+
+// 	},
+// });
