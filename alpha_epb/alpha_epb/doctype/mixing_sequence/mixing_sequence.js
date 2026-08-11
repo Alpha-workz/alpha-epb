db5f5af7960a555ee2f26957bf2a626bc8d89149
rsvasanth <alphaworkz@Alphaworkzs-MacBook-Air-5.local>
2026-08-11 12:58:25 +0530

    feat: align Mixing Sequence doctype with spec sheet
    Add BOM field, split Sequence/BOM Item into separate child tables
    (new Mixing Sequence Step doctype), filter Item Code to Compound
    item group, and replace Parameters Group with a Type select
    (Numeric/Data/Binary/Comment) on Mixing Sequence Parameter.

---
+++ alpha_epb/alpha_epb/doctype/mixing_sequence/mixing_sequence.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, AlphaWorkz and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Mixing Sequence", {
+	setup(frm) {
+		frm.set_query("item_code", () => ({
+			filters: { item_group: "Compound" },
+		}));
+		frm.set_query("bom", () => ({
+			filters: { item: frm.doc.item_code, docstatus: 1, is_active: 1 },
+		}));
+	},
+});
